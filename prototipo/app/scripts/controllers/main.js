537891c08153144645bc74c0bba2d288307cf4f0
Claudio Diaz <clau.diazcubillos@gmail.com>
2015-08-18 23:35:38 -0300

    Carrusel

--- prototipo/app/scripts/controllers/main.js
+++ prototipo/app/scripts/controllers/main.js
@@ -8,7 +8,25 @@
  * Controller of the prototipoApp
  */
 angular.module('prototipoApp')
-  .controller('MainCtrl', function () {
+  .controller('MainCtrl', function ($scope) {
     var baseUrl = 'http://lorempixel.com/960/450/';
     $scope.setInterval=5000;
+
+    $scope.slides = [
+      {
+        tittle: 'Aprende a mantener en forma',
+        image: baseUrl + 'sports/',
+        text: 'Practica algún deporte todos los días'
+      },
+      {
+        tittle: 'Buena Alimentación',
+        image: baseUrl + 'food/',
+        text: 'La importancia de las verdudras y frutas'
+      },
+      {
+        tittle: 'En contacto con la naturaleza',
+        image: baseUrl + 'nature/',
+        text: 'Mantente en armonia con la naturaleza'
+      }
+    ];
   });
