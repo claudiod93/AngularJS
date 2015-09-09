f8b41356355f9b3eb3f9ecdba47d579264b43ff5
claudiod93 <clau.diazcubillos@gmail.com>
2015-09-08 23:37:10 -0300

    Add new fixture

--- prototipo/app/scripts/controllers/miembros.js
+++ prototipo/app/scripts/controllers/miembros.js
@@ -8,12 +8,42 @@
  * Controller of the prototipoApp
  */
 angular.module('prototipoApp')
-  .controller('MiembrosCtrl', function ($scope, $http) {
+  .controller('MiembrosCtrl', function ($scope, $http, $modal) {
       $http.get('http://localhost:9000/miembros.json').success(function(data){
         $scope.miembros = data;
       })
 
     $scope.gridOptions = {
-      data: 'miembros'
+      data: 'miembros',
+      showGroupPanel: true,
+      enableCellSelection: true,
+      enableRowSelection: false,
+      enableCellEdit: true,
+      columnDefs:[
+        {field:'no', displayName: 'Nº'},
+        {field:'nombre', displayName: 'Nombre'},
+        {field:'fidelidad', displayName: 'Puntos Fidelidad'},
+        {field:'fechaUnion', displayName: 'Fecha de Unión'},
+        {field:'tipoMiembro', displayName: 'Tipo de Miembro'}
+      ]
     };
+
+    $scope.showModal = function(){
+      $scope.nuevoMiembro={};
+      var modalInstance = $modal.open({
+        templateUrl: 'views/add-miembros.html',
+        controller: 'addNuevoMiembroCtrl'
+      })
+    }
+  })
+
+.controller('addNuevoMiembroCtrl', function ($scope, $modalInstance, nuevoMiembro){
+    $scope.nuevoMiembro = nuevoMiembro;
+    $scope.salvaNuevoMiembro = function(){
+      $modalInstance.close(nuevoMiembro);
+    }
+
+    $scope.cancel = function(){
+      $modalInstance.dismiss('cancel');
+    }
   });
