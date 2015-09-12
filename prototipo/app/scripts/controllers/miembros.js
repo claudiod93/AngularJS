01c21cf3bdf3e097e80444eb3aed207a3903f21b
claudiod93 <clau.diazcubillos@gmail.com>
2015-09-11 21:22:15 -0300

    Add valid real time

--- prototipo/app/scripts/controllers/miembros.js
+++ prototipo/app/scripts/controllers/miembros.js
@@ -8,42 +8,59 @@
  * Controller of the prototipoApp
  */
 angular.module('prototipoApp')
-  .controller('MiembrosCtrl', function ($scope, $http, $modal) {
-      $http.get('http://localhost:9000/miembros.json').success(function(data){
-        $scope.miembros = data;
-      })
-
+  .controller('MiembrosCtrl', function ($scope,$http, $modal) {
+    $http.get('http://localhost:9000/miembros.json').success(function(data){
+      $scope.miembros =data
+    })
     $scope.gridOptions = {
       data: 'miembros',
       showGroupPanel: true,
       enableCellSelection: true,
       enableRowSelection: false,
       enableCellEdit: true,
-      columnDefs:[
-        {field:'no', displayName: 'Nº'},
-        {field:'nombre', displayName: 'Nombre'},
-        {field:'fidelidad', displayName: 'Puntos Fidelidad'},
-        {field:'fechaUnion', displayName: 'Fecha de Unión'},
-        {field:'tipoMiembro', displayName: 'Tipo de Miembro'}
-      ]
+      columnDefs: [
+        {field:'no', displayName:'Nº.'},
+        {field:'nombre', displayName:'Nombre'},
+        {field:'fidelidad', displayName:'Puntos Fidelidad'},
+        {field:'fechaUnion', displayName:'Fecha de Unión'},
+        {field:'tipoMiembro', displayName:'Tipo de Miembro'}]
     };
 
-    $scope.showModal = function(){
+    $scope.showModal=function () {
       $scope.nuevoMiembro={};
       var modalInstance = $modal.open({
         templateUrl: 'views/add-miembros.html',
-        controller: 'addNuevoMiembroCtrl'
-      })
-    }
+        controller:'AddNuevoMiembroCtrl',
+        resolve: {
+          nuevoMiembro: function() {
+            return $scope.nuevoMiembro;
+          }
+        }
+      });
+
+      modalInstance.result.then(function(selectedItem) {
+        $scope.miembros.push({
+          no: $scope.miembros.length + 1,
+          nombre: $scope.nuevoMiembro.nombre,
+          tipoMiembro: $scope.nuevoMiembro.tipoMiembro,
+          fidelidad: $scope.nuevoMiembro.fidelidad,
+          fechaUnion: $scope.nuevoMiembro.fechaUnion
+
+        });
+      });
+    };
+
   })
-
-.controller('addNuevoMiembroCtrl', function ($scope, $modalInstance, nuevoMiembro){
-    $scope.nuevoMiembro = nuevoMiembro;
-    $scope.salvaNuevoMiembro = function(){
+  .controller('AddNuevoMiembroCtrl', function ($scope, $modalInstance,nuevoMiembro) {
+    $scope.nuevoMiembro=nuevoMiembro;
+    $scope.salvarNuevoMiembro=function(){
       $modalInstance.close(nuevoMiembro);
-    }
-
-    $scope.cancel = function(){
+    };
+    $scope.cancel =function(){
       $modalInstance.dismiss('cancel');
-    }
+    };
   });
+
+
+
+
